3bfc99ec54783772283f83f68c5746d863ff508e
cybiz <kieron@tig.com.au>
2021-09-27 08:57:52 +1000

    2nd cont bgd yellow

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
 
 <body is="dmx-app" id="index">
     <?php include 'nav.php'; ?>
-<div class="container mt-5 bg-primary">
+<div class="container mt-5 bg-warning">
 <div class="row">
 <div class="col-12 col-md">
 <img src="assets/img/hongkong.webp" class="img-fluid" width="600px">
