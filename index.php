e164a6c7db8d5b6c9f195487e2de4faae5fbe623
cybiz <kieron@tig.com.au>
2021-09-27 09:04:54 +1000

    main hdg color & style

--- index.php
+++ index.php
@@ -23,7 +23,7 @@
 
 </div>
 <div class="col-12 offset-md-0 col-md">
-<h1 class="text-center">Fancy display heading</h1>
+<h1 class="text-center display-1 fw-bold text-white">Fancy display heading</h1>
 <h3 class="text-center">Fancy display heading</h3>
 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
 </div>
